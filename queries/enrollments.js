7e9030d911c86abc3a752db0eeda4694e0fd4a59
mohsensami <mohsensami18727@gmail.com>
2026-05-04 18:48:22 +0330

    add enrollment-model and add enrollments adn testimonials queriy

---
+++ queries/enrollments.js
@@ -0,0 +1,10 @@
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/lib/convertData";
+import { Enrollment } from "@/model/enrollment-model";
+
+export async function getEnrollmentsForCourse(courseId) {
+  const enrollments = await Enrollment.find({ course: courseId }).lean();
+  return replaceMongoIdInArray(enrollments);
+}
