2205766885da52dbd3aadbe5687be358b2a2ea00
mohsensami <mohsensami18727@gmail.com>
2026-05-06 20:55:49 +0330

    install next-auth rout.js and login-form

--- app/login/_components/login-form.jsx
+++ app/login/_components/login-form.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -10,8 +11,32 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ceredntialLogin } from "@/app/actions";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function LoginForm() {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await ceredntialLogin(formData);
+
+      if (!!response.error) {
+        console.log(response.error);
+        setError(response.error);
+      } else {
+        router.push("/courses");
+      }
+    } catch (e) {
+      setError(e.message);
+    }
+  }
+
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader>
@@ -28,35 +53,38 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              {/* <Link href="#" className="ml-auto inline-block text-sm underline">
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                {/* <Link href="#" className="ml-auto inline-block text-sm underline">
                 Forgot your password?
               </Link> */}
+              </div>
+              <Input id="password" name="password" type="password" required />
             </div>
-            <Input id="password" type="password" required />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
           </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="register" className="underline">
-            Register
-          </Link>
-        </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="register" className="underline">
+              Register
+            </Link>
+          </div>
+        </form>
       </CardContent>
     </Card>
   );
