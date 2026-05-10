f29502b0560ff480b8e19a11d96fdb9d93ab901a
mohsensami <mohsensami18727@gmail.com>
2026-05-10 21:46:57 +0330

    updated changePassword action

--- app/actions/account.js
+++ app/actions/account.js
@@ -1,7 +1,9 @@
 "use server";
 
 import { User } from "@/model/user-model";
+import { validatePassword } from "@/queries/users";
 import { revalidatePath } from "next/cache";
+import bcrypt from "bcryptjs";
 
 export async function updateUserInfo(email, updatedData) {
   try {
@@ -13,3 +15,24 @@ export async function updateUserInfo(email, updatedData) {
   }
 }
 // End method
+
+export async function changePassword(email, oldPassword, newPassword) {
+  const isMatch = await validatePassword(email, oldPassword);
+
+  if (!isMatch) {
+    throw new Error("Please enter a valid current password");
+  }
+  const filter = { email: email };
+  const hashedPassword = await bcrypt.hash(newPassword, 5);
+
+  const dataToUpadate = {
+    password: hashedPassword,
+  };
+
+  try {
+    await User.findOneAndUpdate(filter, dataToUpadate);
+    revalidatePath("/account");
+  } catch (error) {
+    throw new Error(error);
+  }
+}
