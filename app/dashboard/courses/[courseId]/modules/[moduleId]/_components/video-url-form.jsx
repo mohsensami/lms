3e81230aaa6236cdd28c60fe02601b37c58ce827
mohsensami <mohsensami18727@gmail.com>
2026-05-22 16:05:10 +0330

    update video lesson url

--- app/dashboard/courses/[courseId]/modules/[moduleId]/_components/video-url-form.jsx
+++ app/dashboard/courses/[courseId]/modules/[moduleId]/_components/video-url-form.jsx
@@ -20,6 +20,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { VideoPlayer } from "@/components/video-player";
 import { formatDuration } from "@/lib/date";
+import { updateLesson } from "@/app/actions/lesson";
 
 const formSchema = z.object({
   url: z.string().min(1, {
@@ -43,16 +44,27 @@ export const VideoUrlForm = ({ initialData, courseId, lessonId }) => {
 
   const form = useForm({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: state,
   });
 
   const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values) => {
     try {
-      toast.success("Lesson updated");
-      toggleEdit();
-      router.refresh();
+      const payload = {};
+      payload["video_url"] = values?.url;
+      const duration = values?.duration;
+      const splitted = duration.split(":");
+      if (splitted.length === 3) {
+        payload["duration"] =
+          splitted[0] * 3600 + splitted[1] * 60 + splitted[2] * 1;
+        await updateLesson(lessonId, payload);
+        toast.success("Lesson updated");
+        toggleEdit();
+        router.refresh();
+      } else {
+        toast.error("The duration format must be as hh:mm:ss");
+      }
     } catch {
       toast.error("Something went wrong");
     }
@@ -75,11 +87,9 @@ export const VideoUrlForm = ({ initialData, courseId, lessonId }) => {
       </div>
       {!isEditing && (
         <>
-          <p className="text-sm mt-2">
-            {"https://www.youtube.com/embed/LJi2tiWiYmI?si=-vs8fO-xzWmu7ztG"}
-          </p>
+          <p className="text-sm mt-2">{state?.url}</p>
           <div className="mt-6">
-            <VideoPlayer />
+            <VideoPlayer url={state?.url} />
           </div>
         </>
       )}
