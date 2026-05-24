564ce9ae06a5bd9efad612e00444c9778bf85c5a
mohsensami <mohsensami18727@gmail.com>
2026-05-24 21:38:19 +0330

    add module-action component and edit module action

--- app/dashboard/courses/[courseId]/modules/[moduleId]/page.jsx
+++ app/dashboard/courses/[courseId]/modules/[moduleId]/page.jsx
@@ -14,9 +14,11 @@ import { CourseActions } from "../../_components/course-action";
 import { getModule } from "@/queries/modules";
 import { replaceMongoIdInArray } from "@/lib/convertData";
 import { ObjectId } from "mongoose";
+import { ModuleActions } from "./_components/module-action";
 
 const Module = async ({ params: { courseId, moduleId } }) => {
   const module = await getModule(moduleId);
+  const sanitizedModule = sanitizeData(module);
   // console.log(module);
   function sanitizeData(data) {
     return JSON.parse(
@@ -55,7 +57,7 @@ const Module = async ({ params: { courseId, moduleId } }) => {
               Back to course setup
             </Link>
             <div className="flex items-center justify-end">
-              <CourseActions />
+              <ModuleActions module={sanitizedModule} courseId={courseId} />
             </div>
           </div>
         </div>
