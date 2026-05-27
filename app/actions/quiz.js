36414b331a05003c2842bc6854d641b55f251b85
mohsensami <mohsensami18727@gmail.com>
2026-05-27 19:01:38 +0330

    update quizsets

---
+++ app/actions/quiz.js
@@ -0,0 +1,10 @@
+"use server";
+import { Quizset } from "@/model/quizset-model";
+
+export async function updateQuizSet(quizset, dataToUpdate) {
+  try {
+    await Quizset.findByIdAndUpdate(quizset, dataToUpdate);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
