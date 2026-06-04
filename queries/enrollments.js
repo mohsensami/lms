9c5b2c07148cc06fd40b28414da9dc688a6c51a6
mohsensami <mohsensami18727@gmail.com>
2026-06-04 11:28:19 +0330

    leason authorization gaurd 2

--- queries/enrollments.js
+++ queries/enrollments.js
@@ -39,3 +39,23 @@ export async function getEnrollmentsForUser(userId) {
     throw new Error(err);
   }
 }
+
+export async function hasEnrollmentForCourse(courseId, studentId) {
+  try {
+    const enrollment = await Enrollment.findOne({
+      course: courseId,
+      student: studentId,
+    })
+      .populate({
+        path: "course",
+        model: Course,
+      })
+      .lean();
+
+    if (!enrollment) return false;
+
+    return true;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
