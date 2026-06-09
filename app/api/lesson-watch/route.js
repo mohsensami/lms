74b9c65a13a45c8fea9d1c20e49bbd407e521faf
mohsensami <mohsensami18727@gmail.com>
2026-06-09 22:01:31 +0330

    updated lesson-watch route for completed

--- app/api/lesson-watch/route.js
+++ app/api/lesson-watch/route.js
@@ -1,4 +1,5 @@
 import { getLoggedInUser } from "@/lib/loggedin-user";
+import { Watch } from "@/model/watch-model";
 import { getLesson } from "@/queries/lessons";
 import { getModuleBySlug } from "@/queries/modules";
 import { NextRequest, NextResponse } from "next/server";
@@ -28,6 +29,50 @@ export async function POST(request) {
 
   if (!lesson) {
     return new NextResponse(`Invalid lesson. Can not process request`, {
+      status: 500,
+    });
+  }
+
+  const watchEntry = {
+    lastTime,
+    lesson: lesson.id,
+    module: module.id,
+    user: loggedinUser.id,
+    state,
+  };
+
+  try {
+    const found = await Watch.findOne({
+      lesson: lessonId,
+      module: module.id,
+      user: loggedinUser.id,
+    }).lean();
+
+    if (state === STARTED) {
+      if (!found) {
+        watchEntry["created_at"] = Date.now();
+        await Watch.create(watchEntry);
+      }
+    } else if (state === COMPLETED) {
+      if (!found) {
+        watchEntry["created_at"] = Date.now();
+        await Watch.create(watchEntry);
+      } else {
+        if (found.state === STARTED) {
+          watchEntry["modified_at"] = Date.now();
+          await Watch.findByIdAndUpdate(found._id, {
+            state: COMPLETED,
+          });
+        }
+      }
+    }
+
+    return new NextResponse("Watch Record added Successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(error.message, {
       status: 500,
     });
   }
