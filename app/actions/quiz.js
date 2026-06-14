9b6ff23ff2f269088dd68851204b783359e50913
mohsensami <mohsensami18727@gmail.com>
2026-06-14 19:30:48 +0330

    update quiz action and reports query

--- app/actions/quiz.js
+++ app/actions/quiz.js
@@ -6,6 +6,7 @@ import { Quiz } from "@/model/quizzes-model";
 import mongoose from "mongoose";
 import { Assessment } from "@/model/assessment-model";
 import { getLoggedInUser } from "@/lib/loggedin-user";
+import { createAssessmentReport } from "@/queries/reports";
 
 export async function updateQuizSet(quizset, dataToUpdate) {
   try {
@@ -129,5 +130,13 @@ export async function addQuizAssessment(courseId, quizSetId, answers) {
 
     const assessment = await Assessment.create(assessmentEntry);
     const loggedInUser = await getLoggedInUser();
-  } catch (error) {}
+
+    await createAssessmentReport({
+      courseId: courseId,
+      userId: loggedInUser.id,
+      quizAssessment: assessment?._id,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
