b687bd8ef963ee2202fd0935b0b7978fae2a3be4
mohsensami <mohsensami18727@gmail.com>
2026-06-18 11:24:38 +0330

    update next.config file and vercel.sjon

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
-  },
+    // حذف distDir یا تغییر به .next
+    // distDir: "build", // این خط را کامنت کنید یا حذف کنید
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.pravatar.cc',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
