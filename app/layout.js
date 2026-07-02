9aae8c8cc06c0edefe1b290bd1b64b6d319aa2fe
mohsensami <mohsensami18727@gmail.com>
2026-07-02 23:43:03 +0330

    add Iransansx font

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/sonner';
 import { dbConnect } from '@/service/mongo';
 import { ProgressProvider } from '@/components/providers/progress-provider';
+import localFont from 'next/font/local';
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,34 @@ export const metadata = {
 
 // const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
 
+const IranSans = localFont({
+    src: [
+        {
+            path: '../public/fonts/IranSansX/Woff2/IRANSansXFaNum-Light.woff2',
+            // path: '../../public/fonts/IranSansX/Woff2/IRANSansXFaNum-Medium.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/IranSansX/Woff2/IRANSansXFaNum-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/IranSansX/Woff2/IRANSansXFaNum-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
+    variable: '--font-sans',
+    display: 'swap',
+});
+
 export default async function RootLayout({ children }) {
     await dbConnect();
 
     return (
-        <html lang="en">
+        <html lang="fa" dir="rtl" className={cn(`antialiased, ${IranSans.variable}`)}>
             <body className={cn(`antialiased, poppins.className`)}>
                 <ProgressProvider>{children}</ProgressProvider>
                 <Toaster richColors position="top-center" />
