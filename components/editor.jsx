8f80ccac4db98306ab5e2ba66464088936c12a13
mohsensami <mohsensami18727@gmail.com>
2026-07-06 16:19:29 +0330

    downgrade react-quill-new for fixi bug vercel

--- components/editor.jsx
+++ components/editor.jsx
@@ -1,19 +1,14 @@
-"use client";
-import { useMemo } from "react";
+'use client';
+import { useMemo } from 'react';
 
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
-
-
+import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.snow.css';
 
 export const Editor = ({ onChange, value }) => {
-  const ReactQuill = useMemo(
-    () => dynamic(() => import("react-quill"), { ssr: false }),
-    []
-  );
-  return (
-    <div className="bg-white">
-      <ReactQuill theme="snow" value={value} onChange={onChange} />
-    </div>
-  );
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill-new'), { ssr: false }), []);
+    return (
+        <div className="bg-white">
+            <ReactQuill theme="snow" value={value} onChange={onChange} />
+        </div>
+    );
 };
--- components/preview.jsx
+++ components/preview.jsx
@@ -1,13 +1,10 @@
-"use client";
-import { useMemo } from "react";
+'use client';
+import { useMemo } from 'react';
 
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.bubble.css";
+import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.bubble.css';
 
 export const Preview = ({ value }) => {
-  const ReactQuill = useMemo(
-    () => dynamic(() => import("react-quill"), { ssr: false }),
-    []
-  );
-  return <ReactQuill theme="bubble" readOnly value={value} />;
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill-new'), { ssr: false }), []);
+    return <ReactQuill theme="bubble" readOnly value={value} />;
 };
